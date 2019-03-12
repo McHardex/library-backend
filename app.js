fcc5eadde0acfc008bcabecd67cbc69c037590e0
Akinremi Olumide <akinremiolumide@Akinremis-MacBook-Pro.local>
2019-03-12 19:11:42 +0100

    [Chore] Project setup

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import chalk from 'chalk';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.all('*', (req, res) => {
+  res.send({ status: 404, error: 'Sorry, the page you tried cannot be found' });
+});
+
+if (!module.parent) {
+  app.listen(port, () => {
+    console.log(`listening on port ${chalk.blue(port)}`);
+  });
+}
+
+export default app;
