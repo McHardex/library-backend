6c61752f7c131e4c2b77a7f87dd0f03d47b905de
Akinremi Olumide <akinremiolumide@Akinremis-MacBook-Pro.local>
2019-03-14 10:53:25 +0100

    [Chore] Added pull request template

---
+++ pull_request_template.md
@@ -0,0 +1,9 @@
+__What does this PR do?__
+
+__Description of Task to be completed?__
+
+__How should this be manually tested?__
+
+__What are the relevant pivotal tracker stories?__
+
+__Screenshots__
--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import chalk from 'chalk';
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
